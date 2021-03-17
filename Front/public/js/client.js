11eaa5ddf96cff70acc4746ae09dda217434737b
DipuMsd07 <dsarode013@gmail.com>
2021-03-17 21:28:51 +0530

    Initial Commit

--- Front/public/js/client.js
+++ Front/public/js/client.js
@@ -47,6 +47,7 @@ socket.on('user-joined', name =>{
 // If server sends a message, receive it
 socket.on('receive', data =>{
     append(`${data.name}: ${data.message}`, 'left')
+    audio.play();
 })
 
 // If a user leaves the chat, append the info to the container
